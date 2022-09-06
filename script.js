c1a3364b92278d72c4b881b0b0dc2dbefaf31246
Rob <rtrhoover@gmail.com>
2022-09-06 01:13:49 -0700

    troubleshooting modal launch

--- script.js
+++ script.js
@@ -13,35 +13,43 @@ sidebar.append(modalButton);
 const modal = document.createElement("modal");
 modal.setAttribute("id", "sidebar-modal");
 modal.classList.add("modal");
-modal.setAttribute("style", "position: fixed; z-index: 1; padding-top: 100px; left: 0; top: 0; width: 100%; height: 100%; overflow: auto; background-color: rgba(0,0,0,0.4); opacity: 0; visibility: hidden; transform: scale(1.1)");
+modal.setAttribute("style", "display: none; position: fixed; z-index: 1; padding-top: 100px; left: 0; top: 0; width: 100%; height: 100%; overflow: auto; background-color: rgb(0,0,0); background-color: rgba(0,0,0,0.4); transform: scale(1.1)");
 
 // Create Modal Content
 const modalContent = document.createElement("modal-content");
 modalContent.classList.add("modal-content")
-modalContent.setAttribute("style", "position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: #ffffff; margin: 15% auto; padding: 1rem 1.5rem; border: 1px solid #444b4e; border-radius: 0.5rem");
+modalContent.setAttribute("style", "background-color: #ffffff; margin: auto; padding: 1rem 1.5rem; border: 1px solid #444b4e; border-radius: 0.5rem; width: 80%");
 modalContent.innerHTML = `  
     <div>
-        <span class="close">&times;</span>
+        <span class="close-button">&times;</span>
         <h1>Stuck in the Modal with You</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat praesentium incidunt consequuntur fugit necessitatibus libero dolores unde doloribus pariatur dignissimos debitis maiores, enim sequi fuga, quidem illo nam adipisci quo?</p>
     </div>
 `;
+modal.append(modalContent);
 
-// Button Functionality
-var closeButton = document.getElementsByClassName("close")[0];
-closeButton.setAttribute("style", "float: right; width: 1.5rem; line-height: 1.5rem; text-align: center; cursor: pointer; border-radius: 0.25rem; background-color: #444b4e")
+// Close Button
+var closeButton = modalContent.querySelector(".close-button");
+closeButton.setAttribute("style", "float: right; width: 1.5rem; line-height: 1.5rem; text-align: center; cursor: pointer; border-radius: 0.25rem; background-color: #444b4e");
 
-// Open Modal
-modalButton.onclick = function() {
+modalButton.addEventListener("click", function() {
     modal.style.display = "block";
-}
-// Close Modal by clicking X
-closeButton.onclick = function() {
+});
+closeButton.addEventListener("click", function() {
     modal.style.display = "none";
-}
-// Close Modal by clicking anywhere else in the window
-window.onclick = function(event) {
+});
+window.addEventListener("click", function(event) {
     if (event.target == modal) {
-        modal.style.display = "none"
+        modal.style.display = "none";
     }
-}
+});
+
+// function windowOnClick (event) {
+//     if (event.target === modal) {
+//         toggleModal();
+//     }
+// }
+
+// modalButton.addEventListener("click", toggleModal);
+// closeButton.addEventListener("click", toggleModal);
+// window.addEventListener("click", windowOnClick);
