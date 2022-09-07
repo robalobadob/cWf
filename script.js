23dbd3fde55e21f259eea8c6fb43395db3cac72c
Rob <rtrhoover@gmail.com>
2022-09-07 13:04:04 -0700

    properly aligned css in value prop section

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ sidebar.classList.add("sticky-sidebar");
 // ****   Task 2: MODAL   ****
 
 // Create Modal Button on Sidebar
+const sidebarContent = document.querySelector(".show-on-desktop-min");
 const modalButton = document.createElement("button");
 modalButton.classList.add("modal-button");
 modalButton.innerHTML = "Click Here For A Modal!"
-sidebar.append(modalButton);
+sidebarContent.append(modalButton);
 
 // Create Modal
 const modal = document.createElement("div");
@@ -54,7 +55,7 @@ window.onclick = function(event) {
 // Create Section
 const valueProp = document.createElement("div");
 valueProp.setAttribute("id", "value-props");
-valueProp.classList.add("container", "value-container");
+valueProp.classList.add("container", "values-container");
 valueProp.innerHTML = `
     <section class="value-section">
         <h2>Value Propositions</h2>
@@ -64,22 +65,28 @@ valueProp.innerHTML = `
                 <div class="values-image-block">
                     <img class="values-img" src="https://img.icons8.com/external-wanicon-lineal-color-wanicon/344/external-computer-free-time-wanicon-lineal-color-wanicon.png" alt="Computer Icon">
                 </div>
-                <h3 class="values-header">Header 1</h3>
-                <p class="values-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, voluptatibus? Magnam a saepe dolorem ipsa cumque iste laudantium nulla perspiciatis incidunt in, sequi minus provident inventore amet fuga? Omnis, pariatur!</p>
+                <div class="values-text-block">
+                    <h3 class="values-header">Header 1</h3>
+                    <p class="values-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, voluptatibus? Magnam a saepe dolorem ipsa cumque iste laudantium nulla perspiciatis incidunt in, sequi minus provident inventore amet fuga? Omnis, pariatur!</p>
+                </div>
             </div>
             <div class="values-content-block">
                 <div class="values-image-block">
                     <img class="values-img" src="https://img.icons8.com/cute-clipart/344/truck.png" alt="Truck Icon">
                 </div>
-                <h3 class="values-header">Header 2</h3>
-                <p class="values-content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et officiis vero, magnam mollitia inventore iste eaque suscipit placeat! Earum est ducimus vitae ratione! Maiores fuga, reiciendis debitis quae quo veniam.</p>
+                <div class="values-text-block">
+                    <h3 class="values-header">Header 2</h3>
+                    <p class="values-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, voluptatibus? Magnam a saepe dolorem ipsa cumque iste laudantium nulla perspiciatis incidunt in, sequi minus provident inventore amet fuga? Omnis, pariatur!</p>
+                </div>
             </div>
             <div class="values-content-block">
                 <div class="values-image-block">
                     <img class="values-img" src="https://img.icons8.com/bubbles/344/touchscreen-smartphone.png" alt="Phone Icon">
+                </div>
+                <div class="values-text-block">
+                    <h3 class="values-header">Header 3</h3>
+                    <p class="values-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, voluptatibus? Magnam a saepe dolorem ipsa cumque iste laudantium nulla perspiciatis incidunt in, sequi minus provident inventore amet fuga? Omnis, pariatur!</p>
                 </div>    
-                <h3 class="values-header">Header 3</h3>
-                <p class="values-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut consequatur autem quas quaerat saepe inventore alias odit porro rem ipsam. Et impedit repellendus a consequatur nobis veniam iste similique repudiandae?</p>
             </div>
         </div>
     </section>
@@ -96,35 +103,3 @@ leadership.append(valueProp);
 var sheet = document.createElement('style')
 sheet.innerHTML = ``;
 document.body.appendChild(sheet);
-
-
-
-
-
-
-// function mobileModal(x) {
-//     if (x.matches) {
-//         modal.style.padding = "0px";
-//         modalContent.style.margin = "0px"
-//     }
-// };
-
-// var x = window.matchMedia("(max-width: 700px)").matches;
-// mobileModal(x);
-// modal.addEventListener("change", mobileModal);
-
-
-
-// modalButton.addEventListener("click", function() {
-//     modal.style.display = "block";
-//     console.log("clicked")
-//     console.log(modal.style.display)
-// });
-
-// closeButton.addEventListener("click", function() {
-//     modal.style.display = "none";
-//     console.log("clicked")
-//     console.log(modal.style.display)
-// });
-
-// background-color: #444b4e
